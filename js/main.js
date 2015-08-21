22a48c1c4885f836ff036a8e9e56cbdb356595a6
Joshua Harris <joshua.jharris13@gmail.com>
2015-08-21 14:55:00 -0400

    misc

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ var ref = new Firebase(refUrl);
 $(document).ready(function(){
 
   ref.on("value", function(snapshot){
+    $('#chat-log').empty();
+
     var messages = snapshot.val();
     $.each(messages, function(i, message){
       $('#chat-log').append(
         "<div>" +
-          "<b>" + message.author + " - " + message.timestamp + "</b>" +
+          "<b>" + message.author + " @ " + message.timestamp + "</b>" +
           "<p>" + message.text + "</p>" +
         "</div>"
       );
@@ -17,17 +19,30 @@ $(document).ready(function(){
   
   $('#message').keypress(function(e) {
     if (e.charCode === 13) {
-      var currentDate = new Date();
-      var time = currentDate.getDate() + "/" +
-                 (currentDate.getMonth() + 1) + "/" +
-                 currentDate.getFullYear() + " @ " +
-                 currentDate.getHours() + ":" +
-                 currentDate.getMinutes() + ":" +
-                 currentDate.getSeconds();
-      ref.push({author: $('#username').val(), timestamp: time, text:$('#message').val()});
-      $('#message').val('');
+      if($("#username").val().length > 0) {
+        var currentDate = new Date();
+        var time = //currentDate.getDate() + "/" +
+                   //(currentDate.getMonth() + 1) + "/" +
+                   //currentDate.getFullYear() + " @ " +
+                   currentDate.getHours() + ":" +
+                   currentDate.getMinutes() + ":" +
+                   currentDate.getSeconds();
+        ref.push({author: $('#username').val(), timestamp: time, text:$('#message').val()});
+        $('#message').val('');
+        $('#errors').empty();
+      } else {
+        postError("You must set your name");
+      }
       return false;
     }
   });
+
+  function postError(errorMsg) {
+    $('#errors').append(
+      '<div class="alert alert-danger">' +
+        errorMsg +
+      '</div>'
+    );
+  }
 
 });
