1f5de8546c866b23b3f0de6bc7aa4024e706f351
Joshua Harris <joshua.jharris13@gmail.com>
2015-09-01 20:26:24 -0400

    fixed

--- js/main.js
+++ js/main.js
@@ -8,53 +8,51 @@ $(document).ready(function(){
       console.log("Login Failed!", error);
     } else {
       console.log("Authenticated successfully with payload:", authData);
-
-      ref.on("value", function(snapshot){
-        $('#chat-log').empty();
-
-        var messages = snapshot.val();
-        $.each(messages, function(i, message){
-          $('#chat-log').append(
-            "<div>" +
-              "<b>" + message.author + " @ " + message.timestamp + "</b>" +
-              "<p>" + message.text + "</p>" +
-            "</div>"
-          );
-        });
-        $('#chat-log').animate({ scrollTop: $('#chat-log')[0].scrollHeight}, 1000);
-      });
-      
-      $('#message').keypress(function(e) {
-        if (e.charCode === 13) {
-          if($("#username").val().length > 0) {
-            var currentDate = new Date();
-            var time = //currentDate.getDate() + "/" +
-                       //(currentDate.getMonth() + 1) + "/" +
-                       //currentDate.getFullYear() + " @ " +
-                       currentDate.getHours() + ":" +
-                       currentDate.getMinutes() + ":" +
-                       currentDate.getSeconds();
-            //ref.push({author: $('#username').val(), timestamp: time, text:$('#message').val()});
-            ref.push({author: authData.github.username, timestamp: time, text:$('#message').val()});
-            $('#message').val('');
-            $('#errors').empty();
-          } else {
-            postError("You must set your name");
-          }
-          return false;
-        }
-      });
-
-      function postError(errorMsg) {
-        $('#errors').append(
-          '<div class="alert alert-danger">' +
-            errorMsg +
-          '</div>'
-        );
-      }
-
     }
   });
 
+  ref.on("value", function(snapshot){
+    $('#chat-log').empty();
+
+    var messages = snapshot.val();
+    $.each(messages, function(i, message){
+      $('#chat-log').append(
+        "<div>" +
+          "<b>" + message.author + " @ " + message.timestamp + "</b>" +
+          "<p>" + message.text + "</p>" +
+        "</div>"
+      );
+    });
+    $('#chat-log').animate({ scrollTop: $('#chat-log')[0].scrollHeight}, 1000);
+  });
+  
+  $('#message').keypress(function(e) {
+    if (e.charCode === 13) {
+      if($("#username").val().length > 0) {
+        var currentDate = new Date();
+        var time = //currentDate.getDate() + "/" +
+                   //(currentDate.getMonth() + 1) + "/" +
+                   //currentDate.getFullYear() + " @ " +
+                   currentDate.getHours() + ":" +
+                   currentDate.getMinutes() + ":" +
+                   currentDate.getSeconds();
+        //ref.push({author: $('#username').val(), timestamp: time, text:$('#message').val()});
+        ref.push({author: authData.github.username, timestamp: time, text:$('#message').val()});
+        $('#message').val('');
+        $('#errors').empty();
+      } else {
+        postError("You must set your name");
+      }
+      return false;
+    }
+  });
+
+  function postError(errorMsg) {
+    $('#errors').append(
+      '<div class="alert alert-danger">' +
+        errorMsg +
+      '</div>'
+    );
+  }
 
 });
